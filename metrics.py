e1f98831cb7db5f83f46f1f16fe2231109006ec7
TUDelftHao <cqu.friedrich@gmail.com>
2020-05-13 19:35:05 +0200

    updated

--- metrics.py
+++ metrics.py
@@ -5,6 +5,10 @@ from loss import to_one_hot
 
 def dice_coe(output, target, eps=1e-5):
 
+    '''
+    Used for accuracy evaluation during training and validation 
+    '''
+
     target = to_one_hot(target)
 
     output = output.contiguous().view(output.shape[0], output.shape[1], -1)
@@ -21,54 +25,23 @@ def dice_coe(output, target, eps=1e-5):
     total_dice_coe = (NET_dice_coe + ED_dice_coe + ET_dice_coe)/3
 
     dice_coe = {}
-    dice_coe['avg'] = total_dice_coe
-    dice_coe['BG'] = BG_dice_coe
-    dice_coe['NET'] = NET_dice_coe
-    dice_coe['ED'] = ED_dice_coe
-    dice_coe['ET'] = ET_dice_coe
+    dice_coe['avg'] = total_dice_coe.item()
+    dice_coe['BG'] = BG_dice_coe.item()
+    dice_coe['NET'] = NET_dice_coe.item()
+    dice_coe['ED'] = ED_dice_coe.item()
+    dice_coe['ET'] = ET_dice_coe.item()
     
     return dice_coe
 
-def softmax_output_dice(output, target):
-
-    ''' generate a dict to show dice coefficient of each label'''
-
-    score = []
-    keys = ['tumour core', 'edema', 'enhancing tumour'] # 1, 2, 4
-
-    # dice score of tumour core
-    out = output == 1
-    tar = target == 1
-    score.append(dice_coe(out, tar))
-
-    # dice score of edema
-    out = output == 2
-    tar = target == 2
-    score.append(dice_coe(out, tar))
-
-    # dice score of enhancing tumour
-    out = output == 4
-    tar = target == 4
-    score.append(dice_coe(out, tar))
-
-    dice_dict = {}
-    for value,key in zip(score, keys):
-        dice_dict[key] = value
-
-    return dice_dict
 
 if __name__ == '__main__':
-    import time 
-    now= time.time()
     
     yt = np.random.random(size=(2, 3, 3, 3))
-
     yt = torch.from_numpy(yt)
     yp = np.zeros(shape=(2, 4, 3, 3, 3))
     yp = yp + 1
     yp = torch.from_numpy(yp)
     coe = dice_coe(yp, yt)
     print(coe)
+
     
-    end = time.time() - now
-    print(end)
